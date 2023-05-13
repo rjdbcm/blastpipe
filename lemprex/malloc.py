685fdf9fed68c77105bb5a554578690a5b16074a
rjdbcm <rjdbcm@mail.umkc.edu>
2023-05-13 17:40:35 -0500

    Add proper license attribution.

--- lemprex/malloc.py
+++ lemprex/malloc.py
@@ -1,35 +1,37 @@
-"""Utilities for measuring memory usage.
-    Copyright 2012 Raymond Hettinger
-    Permission is hereby granted, free of charge, to any person obtaining a copy of this software
-    and associated documentation files (the "Software"), to deal in the Software without 
-    restriction, including without limitation the rights to use, copy, modify, merge, publish,
-    distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom 
-    the Software is furnished to do so, subject to the following conditions:
+"""Utilities for measuring memory usage."""
+# Copyright 2012 Raymond Hettinger
+# Permission is hereby granted, free of charge, to any person obtaining a copy of this software
+# and associated documentation files (the "Software"), to deal in the Software without
+# restriction, including without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom
+# the Software is furnished to do so, subject to the following conditions:
 
-    The above copyright notice and this permission notice shall be included in all copies or 
-    substantial portions of the Software.
+# The above copyright notice and this permission notice shall be included in all copies or
+# substantial portions of the Software.
 
-    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
-    EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES 
-    OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. 
-    IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, 
-    DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, 
-    TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE 
-    OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-"""
-from __future__ import print_function
-from sys import getsizeof, stderr
-from itertools import chain
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+# EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+# OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,
+# DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE
+# OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+# from __future__ import print_function
 from collections import deque
+from itertools import chain
+from sys import getsizeof, stderr
+
 try:
     # pylint: disable=redefined-builtin
     from reprlib import repr
-except ImportError:
+except ImportError: # pragma: no cover
     pass
+
 
 # pylint: disable=unnecessary-lambda-assignment
 def total_size(obj, handlers=None, verbose=False):
-    """ Returns the approximate memory footprint an object and all of its contents.
+    """Returns the approximate memory footprint an object and all of its contents.
     https://code.activestate.com/recipes/577504/
 
     Automatically finds the contents of the following builtin containers and
@@ -43,19 +45,20 @@ def total_size(obj, handlers=None, verbose=False):
     if handlers is None:
         handlers = {}
     dict_handler = lambda d: chain.from_iterable(d.items())
-    all_handlers = {tuple: iter,
-                    list: iter,
-                    deque: iter,
-                    dict: dict_handler,
-                    set: iter,
-                    frozenset: iter,
-                   }
-    all_handlers.update(handlers)     # user handlers take precedence
-    seen = set()                      # track which object id's have already been seen
-    default_size = getsizeof(0)       # estimate sizeof object without __sizeof__
+    all_handlers = {
+        tuple: iter,
+        list: iter,
+        deque: iter,
+        dict: dict_handler,
+        set: iter,
+        frozenset: iter,
+    }
+    all_handlers.update(handlers)  # user handlers take precedence
+    seen = set()  # track which object id's have already been seen
+    default_size = getsizeof(0)  # estimate sizeof object without __sizeof__
 
     def sizeof(obj):
-        if id(obj) in seen:       # do not double count the same object
+        if id(obj) in seen:  # do not double count the same object
             return 0
         seen.add(id(obj))
         s = getsizeof(obj, default_size)
