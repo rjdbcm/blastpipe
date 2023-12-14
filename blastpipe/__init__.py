ecaad6f00be73c0d9a4354ab60fa5f3809fc31e1
rjdbcm <rjdbcm@mail.umkc.edu>
2023-12-13 20:16:48 -0600

    Bump for OZI changes >0.0.235

--- blastpipe/__init__.py
+++ blastpipe/__init__.py
@@ -13,37 +13,14 @@
 # KIND, either express or implied.  See the License for the
 # specific language governing permissions and limitations
 # under the License.
-import platform
 import sys
-from datetime import date, datetime, timezone
 from importlib.metadata import PackageNotFoundError, version
-from typing import Annotated, Any, Final
-from warnings import warn
+from typing import Annotated, Any
 
 try:
     __version__ = version('blastpipe')
 except PackageNotFoundError:  # pragma: no cover
     pass
-
-__pymajor, __pyminor, __pypatch = map(int, platform.python_version_tuple())
-
-PYMAJOR: Final[int] = __pymajor
-PYMINOR: Final[int] = __pyminor
-PYPATCH: Final[int] = __pypatch
-
-minor_deprecation = {
-    9: date(2025, 10, 1),
-    10: date(2026, 10, 1),
-    11: date(2027, 10, 1),
-    12: date(2028, 10, 1),
-}
-python3_eol = minor_deprecation.get(PYMINOR, date(2008, 12, 3))
-
-if datetime.now(tz=timezone.utc).date() > python3_eol:  # pragma: no cover
-    warn(
-        f'Python {PYMAJOR}.{PYMINOR}.{PYPATCH} is not supported as of {python3_eol}.',
-        RuntimeWarning,
-    )
 
 
 def public(obj: Any) -> Annotated[Any, '__all__.__contains__(__name__)']:
